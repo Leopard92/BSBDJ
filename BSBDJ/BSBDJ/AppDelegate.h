44719f78e9a5a10238baaf060bcd6e5e9eb12c2a
Leopard92 <yanjingguan@hotmail.com>
2018-07-10 14:50:51 +0800

    初始化项目

---
+++ BSBDJ/BSBDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBDJ
+//
+//  Created by 石原さとみ on 2018/7/10.
+//  Copyright © 2018年 YJG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSBDJ/BSBDJ/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BSBDJ
+//
+//  Created by 石原さとみ on 2018/7/10.
+//  Copyright © 2018年 YJG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
